d50bad237bcf919929f93c8cfbedfa5d22376bc3
Pengfei-cai <18342672548@163.com>
2020-04-10 15:10:32 +0800

    取文件的后缀

---
+++ getSuffix.py
@@ -0,0 +1,8 @@
+def get_suffix(filename,has_dot=False):
+	pos = filename.rfind('.')
+	if 0 < pos < len(filename) - 1:
+		index = pos if has_dot else pos + 1
+		print(filename[index:])
+	else:
+		return ''
+get_suffix('asdasdada1.py')
